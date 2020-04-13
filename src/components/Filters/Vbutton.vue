9cfbecbfb72d2028a477b94e4eb782a5b1f13fb1
artyomnersisyan86 <artyom.nersisyan86@gmail.com>
2020-04-13 20:47:08 +0400

    add Vbutton Component

---
+++ src/components/Filters/Vbutton.vue
@@ -0,0 +1,51 @@
+<template>
+	<div >
+		<v-btn  color="pink" width="200"   outlined dark
+						class="text-capitalize caption justify-start" height="32"
+						@click="click"
+		>
+			<v-icon class="text-left" color="dark" size="24" left>{{icon}}</v-icon>
+			<span  style="margin: 0 auto;" class="text-center">{{label}}</span>	</v-btn>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: "Vbutton",
+		props:{
+			icon:{
+				type:String,
+				default:"mdi-autorenew",
+			} ,
+			id:{
+				type:Number,
+				default:1
+
+			},
+			label: {
+				type:String,
+				default:"Նորացնել"
+			},
+			disabled:{
+				type:Boolean,
+				default:false,
+			},
+
+			className:{
+				type:String,
+				default:"",
+			},
+			// fType: "button"
+		},
+		methods:{
+			click(){
+				this.$emit('click')
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
